c8e79323b4ea671e31a2755e7b2ccd2e7771db62
Ahmed Adel <ahmedadel0239@gmail.com>
2024-02-24 06:53:42 +0200

    My New Extension

---
+++ extension.js
@@ -0,0 +1,51 @@
+const vscode = require("vscode");
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+
+function activate(context) {
+  let disposable = vscode.commands.registerCommand(
+    "copy-syntax-in-light.copySyntaxInLight",
+    async function () {
+      try {
+        // Get the current configuration
+        const config = vscode.workspace.getConfiguration();
+
+        // Set the color theme to "Default Light+"
+        await config.update(
+          "workbench.colorTheme",
+          "Default Light+",
+          vscode.ConfigurationTarget.Global
+        );
+
+        // Copy Selected Text
+        await vscode.commands.executeCommand(
+          "editor.action.clipboardCopyAction"
+        );
+
+        // Show success message
+        vscode.window.showInformationMessage("Text copied in Light Mode");
+
+        // Switch back to dark theme
+        await config.update(
+          "workbench.colorTheme",
+          "Default Dark+",
+          vscode.ConfigurationTarget.Global
+        );
+      } catch (error) {
+        vscode.window.showErrorMessage(`Error: ${error}`);
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
